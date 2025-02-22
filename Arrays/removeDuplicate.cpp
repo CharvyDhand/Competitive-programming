02e8646389f77efd46d738fb593aa283a23ec6f7
CharvyDhand <charvydhand13@gmail.com>
2025-02-22 21:36:47 +0530

    update

--- Arrays/removeDuplicate.cpp
+++ Arrays/removeDuplicate.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
 #include<set>
 using namespace std;
+//OPTIMAL
+//TIME=O(N)
+//SPACE =O(1)
+int main(){
+    int a[6];
+    int n;
+    cout<<"Enter array size:";
+    cin>>n;
+    cout<<"Enter array elements:";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int i=0;
+    for(int j=1;j<n;j++){
+        if(a[j]!=a[i]){
+            a[i+1]=a[j];
+            i++;
+        }
+    }
+    for(int j=0;j<i+1;j++){
+        cout<<a[j]<<" ";
+    }
+}
+
+
 
 //BRUTE SOLUTION
 // TIME COM= 0(NLOGN+N)
