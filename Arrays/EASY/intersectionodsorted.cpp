5fc44e92de41d6ca679f9eb646189809f31b96f6
CharvyDhand <charvydhand13@gmail.com>
2025-02-25 21:18:40 +0530

    commit

--- Arrays/EASY/intersectionodsorted.cpp
+++ Arrays/EASY/intersectionodsorted.cpp
@@ -3,7 +3,35 @@
 #include<set>
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> optimal(vector<int>)
+vector<int> intersectionOfArrays(vector<int>A, vector<int>B){
+    
+    vector <int> ans;
+    
+    // to maintain visited
+    vector <int> visited(B.size(), 0);  
+    int i = 0, j = 0;
+    
+    for (i = 0; i < A.size(); i++) {
+        
+      for (j = 0; j < B.size(); j++) {
+  
+        if (A[i] == B[j] && visited[j] == 0) { 
+        
+          //if element matches and has not been matched with any other before
+          ans.push_back(B[j]);
+          visited[j] = 1;
+  
+          break;
+        } 
+        
+        else if (B[j] > A[i]) break; 
+        //because array is sorted , element will not be beyond this
+      }
+    }
+    
+    return ans;
+      
+  }
 int main(){
     int n,m;
     cout<<"enter size of array 1 and 2:";
@@ -18,8 +46,10 @@ int main(){
     for(int i=0;i<m;i++){
         cin>>b[i];
    }
-   vector<int> set=optimal(a,b);
-   for(int i=0;i<set.size();i++){
-    cout<<set[i]<<" ";
+  
+   vector<int> ans = intersectionOfArrays(a,b);
+  
+   for (int i = 0; i < ans.size(); i++) {
+     cout << ans[i] << " ";
    }
 }
