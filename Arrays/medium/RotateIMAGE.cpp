4d27edcd4d0c8f70d338ebea54d5d3e4c59b8d39
CharvyDhand <charvydhand13@gmail.com>
2025-03-16 23:10:20 +0530

    update

--- Arrays/medium/RotateIMAGE.cpp
+++ Arrays/medium/RotateIMAGE.cpp
@@ -3,18 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 //optimal
-void rotate(vector<vector<int>>& a) {
-    int n= a.size();
-    int m=a[0].size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-        swap(a[i][j],a[j][i]);
+    void rotate(vector<vector<int>>& a) {
+        int n= a.size();
+        int m=a[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+            swap(a[i][j],a[j][i]);
+        }
     }
-}
-for(int i=0;i<n;i++){
-   reverse(a[i].begin(),a[i].end());
-}
-}
+    for(int i=0;i<n;i++){
+       reverse(a[i].begin(),a[i].end());
+    }
+    }
 //brute 
 // void rotate(vector<vector<int>>& a) {
 //     int n= a.size();
