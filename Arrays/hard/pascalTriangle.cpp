33fd85fa0d3c916cdcd883b163d088187dc5186e
CharvyDhand <charvydhand13@gmail.com>
2025-03-19 14:04:14 +0530

    update

--- Arrays/hard/pascalTriangle.cpp
+++ Arrays/hard/pascalTriangle.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<bits/stdc++.h>
 vector<vector<int>> generate(int numRows) {
     vector<vector<int>> a(numRows);
     for(int i=0;i<numRows;i++){
@@ -11,4 +15,34 @@ vector<vector<int>> generate(int numRows) {
         }
     }
     return a;
+}
+int nCr(int n,int r){
+    long long res=1;
+    for(int i=0;i<r;i++){
+        res=res*(n-i);
+        res=res/(i+1);
+    }
+    return (int)(res);
+}
+vector<vector<int>> approach2(int num){
+    vector<vector<int>> ans;
+    for(int i=1;i<=num;i++){
+        vector<int> temp;
+        for(int j=1;j<=i;j++){
+            temp.push_back(nCr(i-1,j-1));
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
+int main(){
+  int r=5;
+//   vector<vector<int>> ans=generate(r);
+vector<vector<int>> ans=approach2(r);
+  for(int i=0;i<r;i++){
+    for(int j=0;j<=i;j++){
+        cout<<ans[i][j]<<" ";
+    }
+    cout<<endl;
+  }
 }
