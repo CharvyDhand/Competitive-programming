0761f75ed57622abb1a58980be7e83d20a94a962
CharvyDhand <charvydhand13@gmail.com>
2025-04-03 17:03:02 +0530

    update

--- Arrays/hard/mergeintervals.cpp
+++ Arrays/hard/mergeintervals.cpp
@@ -4,6 +4,20 @@ using namespace std;
 #include<bits/stdc++.h>
 vector<vector<int>> merge(vector<vector<int>>& a) {
     int n=a.size();
+    sort(a.begin(),a.end());
+    vector<vector<int>> ans;
+    for(int i=0;i<n;i++){
+      if(ans.empty() || ans.back()[1]<a[i][0]){
+        ans.push_back(a[i]);
+      }
+      else{
+        ans.back()[1]=max(ans.back()[1],a[i][1]);
+      }
+    }   
+    return ans;
+}
+vector<vector<int>> merge(vector<vector<int>>& a) {
+    int n=a.size();
     int m=a[0].size();
     vector<vector<int>> ans;
     for(int i=0;i<n;i++){
